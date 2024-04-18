b348072282ad778a46909fd537769831c608a0ba
caiman48 <caiman48clm@gmail.com>
2024-04-18 12:40:09 -0400

    added script to the index and added DOM elements to the script

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -1,4 +1,17 @@
-var APIKey = "e7499d2dbd56e051e8e02bf1046e361f";
+const API_KEY = "9747fc5f9f0716bfbe8bab5aa1dc7a61";
 
 
 
+// DOM element references
+const locationInput = document.getElementById('locationInput');
+const findWeatherButton = document.getElementById('findWeather');
+const pastSearchesSection = document.getElementById('pastSearches');
+const placeNameElement = document.getElementById('placeName');
+const weatherDateElement = document.getElementById('weatherDate');
+const currentIcon = document.getElementById('currentIcon');
+const tempNowElement = document.getElementById('tempNow');
+const windNowElement = document.getElementById('windNow');
+const humidityNowElement = document.getElementById('humidityNow');
+const forecastContainer = document.getElementById('forecastContainer');
+
+// Function to handle city searches
